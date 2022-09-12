f9e2a171c4018f2a6ec91faa31ae8c6df1c71448
jsb0095 <anfzhd0095@gmail.com>
2022-09-12 22:13:59 +0900

    Revert "검색기능 수정"
    This reverts commit 5a73e1d024bb1bfbfede91fed83af2b94fc7150f.

--- src/main/java/com/example/jnj/repository/AlarmRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.jnj.repository;
-
-import com.example.jnj.entity.Alarm;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlarmRepository extends JpaRepository<Alarm,Long> {
-}
